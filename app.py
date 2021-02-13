4a0b0bc9c78c2c185d878d9596ee1a5801d890bc
Muhammed Ashiq Abdul Khader <ashiqabdulkhader@gmail.com>
2021-02-13 19:45:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, Markup
 import requests, json
 
-RUN_URL = 'https://simucode.loca.lt/compile/'
+RUN_URL = 'http://4318fc315a5b.ngrok.io/compile/'
 
 app = Flask(__name__)
 
