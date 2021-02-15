6cadc90ee9d3a35dfeda6d5dbae2bdbd7abab0be
AshiqAbdulkhader <ashiqabdulkhader@gmail.com>
2021-02-15 19:30:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, Markup
 import requests, json
 
-RUN_URL = 'http://24b38f286c2d.ngrok.io/compile/'
+RUN_URL = 'http://cc38f0592716.ngrok.io/compile/'
 
 app = Flask(__name__)
 
