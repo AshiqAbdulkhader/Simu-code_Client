7c3cc05dc1a41afb6e7d6c943a20d30b35e17a57
AshiqAbdulkhader <ashiqabdulkhader@gmail.com>
2021-05-18 15:01:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from formspages import LoginForm
 import requests, json
 from validate import auth
 
-# RUN_URL = 'http://13.234.251.104:8000/compile/'
-RUN_URL = 'http://127.0.0.1:8000/compile/'
+RUN_URL = 'http://35.154.94.183:8000/compile/'
+# RUN_URL = 'http://127.0.0.1:8000/compile/'
 
 app = Flask(__name__)
 
