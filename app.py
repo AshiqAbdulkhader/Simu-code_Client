e0ded3d05777e577523ee0895dcd258353576e35
Muhammed Ashiq Abdul Khader <ashiqabdulkhader@gmail.com>
2022-01-15 10:41:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, flash, redirect, Markup, request, session
 from flask.helpers import url_for
 from formspages import LoginForm
-import requests, json
+import requests
+import json
 from validate import auth
 
 
@@ -12,25 +13,28 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '908449bd184d0c4d6ee71af64324610e'
 
+
 @app.route("/", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         uname = request.form['username']
         pwd = request.form['password']
-        if auth(uname,pwd):
+        if auth(uname, pwd):
             if 'uid' in session and session['uid'] == uname:
-                return redirect(url_for('index')) #Need to handle this better
+                return redirect(url_for('index'))  # Need to handle this better
             else:
                 session['uid'] = uname
                 return redirect(url_for('index'))
-    return render_template('login.html', title='Login', form = form)
+    return render_template('login.html', title='Login', form=form)
+
 
 @app.route('/index')
 def index():
     if 'uid' in session:
         uname = session['uid']
-    return render_template('index.html', uname = uname)
+    return render_template('index.html', uname=uname)
+
 
 @app.route('/index', methods=['POST'])
 def getvalue():
@@ -38,14 +42,15 @@ def getvalue():
     code = request.form['code']
     theme = str(request.form['theme'])
     data = {
-        "source":code,
-        "lang":lang,
-        "id":1,
+        "source": code,
+        "lang": lang,
+        "id": 1,
     }
-    data=json.dumps(data)
+    data = json.dumps(data)
     r = requests.post(RUN_URL, data=data)
     output = Markup(json.loads(r.json())["msg"])
     return render_template('index.html', output=output, code=code, lang=lang, theme=theme)
+
 
 @app.route('/logout')
 def logout():
